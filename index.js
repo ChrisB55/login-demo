edea6c76b7017e7ead863d301783e56501e36531
Christopher W. Brown <christopherwilliambrown@gmail.com>
2018-04-25 15:44:09 -0400

    Update with express, oauth

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const passsport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
 const app = express();
+
+passsport.use(new GoogleStrategy());
 
 app.get("/", (req, res) => {
   res.send({ hello: "world" });
