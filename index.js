ab5d3bda419be194826fa44c6f459f2c364b0bff
Christopher W. Brown <christopherwilliambrown@gmail.com>
2018-04-26 15:07:01 -0400

    Add more configuration

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
-const passsport = require('passport');
+const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
+//Passport JS configuration, including routing directions/CallbackURL for user returning
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken) => {
+  console.log(accessToken);
 
-passsport.use(new GoogleStrategy());
+}));
+
+//google string is in passport, this asks for profile and email from the possible scopes
+app.get('/auth/google',
+ passport.authenticate('google', {
+  scope:['profile', 'email']
+})) 
+
+app.get('/auth/google/callback', passport.authenticate('google'));
+
 
 app.get("/", (req, res) => {
-  res.send({ hello: "world" });
+  res.send({
+    hello: "world"
+  });
 });
 
-const PORT = process.env.PORT || 4000 ;
+const PORT = process.env.PORT || 4000;
 app.listen(PORT);
