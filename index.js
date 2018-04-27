ab1fd45e58c206532e3a024d7ef8b487c1b413a8
Christopher W. Brown <christopherwilliambrown@gmail.com>
2018-04-27 10:36:04 -0400

    Reconfigure file paths

--- index.js
+++ index.js
@@ -1,27 +1,11 @@
 const express = require('express');
-const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('./config/keys');
+require ('./services/passport');
+
+
 
 const app = express();
-//Passport JS configuration, including routing directions/CallbackURL for user returning
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-  console.log(accessToken);
 
-}));
-
-//google string is in passport, this asks for profile and email from the possible scopes
-app.get('/auth/google',
- passport.authenticate('google', {
-  scope:['profile', 'email']
-})) 
-
-app.get('/auth/google/callback', passport.authenticate('google'));
-
+require('./routes/authRoutes')(app);
 
 app.get("/", (req, res) => {
   res.send({
