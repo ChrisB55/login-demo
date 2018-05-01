b2cb2e7e39c87f6b2d4b4afbe2cd75d40f428b53
Christopher W. Brown <christopherwilliambrown@gmail.com>
2018-05-01 11:11:13 -0400

    Add initial database set up

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose= require('mongoose');
+const keys = require('./config/keys');
+require ('./models/User');
 require ('./services/passport');
 
-
-
 const app = express();
+mongoose.connect('mongoURI');
+
+
 
 require('./routes/authRoutes')(app);
 
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
 const passport = require('passport');
+const express = require('express');
 
-
-module.exports = () => {
+module.exports = app => {
     app.get('/auth/google',
     passport.authenticate('google', {
  scope:['profile', 'email']
-})) 
+})); 
 
-app.get('/auth/google/callback', passport.authenticate('google'))
+app.get('/auth/google/callback', passport.authenticate('google'));
 };
